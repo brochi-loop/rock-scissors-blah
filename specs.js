2ae142cf5aa9470360a059ebd303fdb23793a94d
brochi-loop <122704991+brochi-loop@users.noreply.github.com>
2023-02-13 15:22:18 -0500

    estructura base del juego

--- specs.js
+++ specs.js
@@ -1,21 +1,68 @@
-   game();
-    function transformUserInput(opciónDeUsuario){
-        let temp=opciónDeUsuario.toLowerCase();
-        let opciónUsuarioNum
-        if(temp==="piedra"){
-            opciónUsuarioNum = 1;
-        } else if (temp==="papel"){
-            opciónUsuarioNum = 2;
-        } else {
-            opciónUsuarioNum = 3;
-        }
-        return opciónUsuarioNum;
 
-    }
+const buttons = Array.from(document.querySelectorAll('button'));
+console.log(buttons);
+console.log(typeof buttons);
+let computerScore=0;
+    let userScore=0;
+    const scoreScreen= document.querySelector('.score-container');
+    game();
+
+    
+    //console.log(scoreScreen);
+    
+
     function getComputerChoice(){
-        return Math.floor(Math.random()*3) + 1;
-        
+        return Math.floor(Math.random()*3) + 1;    
     }
+
+    //function printScore()
+
+    function getUserOption(e){
+        /*let userOption=parseInt(this.classList.value);
+        let computerOption=getComputerChoice();
+        let roundWinner=playRound(userOption, computerOption);*/
+        let roundWinner=playRound(parseInt(this.classList.value), getComputerChoice());
+        if(roundWinner<0) userScore++;
+        if(roundWinner>0) computerScore++;
+        scoreScreen.textContent=`Usuario va: ${userScore} contra ${computerScore}`;
+        if(userScore===5 || computerScore===5){
+            scoreScreen.textContent="\n Gana uno de los dos";
+            if(prompt("quiere jugar de nuevo")==="si"){
+                userScore=0;
+                computerScore=0;
+                game();
+            }
+        };
+    }
+function game(){
+    buttons.forEach(button => button.addEventListener('click', getUserOption));
+}
+    
+    
+    
+
+    
+
+
+    /*const buttonPiedra=document.querySelector('button.piedra');
+    const buttonPapel=document.querySelector('button.papel');
+    const buttonTijera=document.querySelector('button.tijera');
+
+    buttonPiedra.addEventListener('click', (e) => {
+        console.log("PIEDRA");
+    });
+
+    buttonPapel.addEventListener('click', (e) => {
+        console.log("PAPEL");
+    });
+
+    buttonTijera.addEventListener('click', (e) => {
+        console.log("TIJERA");
+    });
+
+    /*buttonOne.forEach(addEventListener('click', (e) => {
+        console.log(e);
+    }));*/
 
     function playRound(opciónDeUsuario, opciónDeComputador){
         let quien_ganó=0;
@@ -36,7 +83,7 @@
         return quien_ganó;
     }
 
-    function game(){
+    /*function game(){
         let marcador_final=0;
         let marcador_temp=0;
         let inputDeUsuario;
@@ -61,4 +108,4 @@
         } else {
             console.log("Ganador final: Computadora");
         }
-    }
+    }*/
